60ca5f9e3a3a47fd980081e2ce62a88b2969fdc7
Roshan.R <roshanleo953@gmail.com>
2026-09-07 07:07:50 +0530

    Add user input examples in Python intro script

--- 01_python_fundamentals/daily_practice/day01_python_intro.py
+++ 01_python_fundamentals/daily_practice/day01_python_intro.py
@@ -1,1 +1,16 @@
+name = input("Enter your name: ")
+print(f"My name is {name}\nI'm learning Python\nMy goal is to be a Software engineer ")
+
+#Exp 1
+age = int(input("Enter your age: "))
+print(f"I'm {age} years old")
+
+#Exp 2 
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+print(a+b)
+
+#Exp 3 
+name = input("Enter your name: ")
+print(f"My name is {name}\nI'm {age} years old")
 
