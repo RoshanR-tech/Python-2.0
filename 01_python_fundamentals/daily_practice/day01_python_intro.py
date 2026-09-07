3e31c982422300b8cebbcbd95ac9f9594c12d3d4
Roshan.R <roshanleo953@gmail.com>
2026-09-07 06:57:59 +0530

    Update day01_python_intro.py

--- 01_python_fundamentals/daily_practice/day01_python_intro.py
+++ 01_python_fundamentals/daily_practice/day01_python_intro.py
@@ -1,8 +1,1 @@
-name = input() #returs a string as default 
-print(name)
 
-age = int(input()) #returs int vaules 
-print(age)
-
-age = int(input()) #will return "age" instead of number
-print("age")
