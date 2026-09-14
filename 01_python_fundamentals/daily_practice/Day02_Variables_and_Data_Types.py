37682185522269effc0d65c0603f8750387671bf
Roshan.R <roshanleo953@gmail.com>
2026-09-14 22:23:34 +0530

    Update Day02_Variables_and_Data_Types.py

--- 01_python_fundamentals/daily_practice/Day02_Variables_and_Data_Types.py
+++ 01_python_fundamentals/daily_practice/Day02_Variables_and_Data_Types.py
@@ -7,3 +7,12 @@ name = "Roshan.R"
 age = 20
 print(f"{name}\n{age}") #prints the stored value in the variables in 2 different lines
 rint(f"name\nage") #prints the words "name","age" in 2 different lines 
+
+name = "Roshan"
+age = 21
+height = 5.9
+is_student = True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
