8590c0ef8f57c282ecad23249a13fae7426afb75
Roshan.R <roshanleo953@gmail.com>
2026-09-14 21:57:27 +0530

    Update Day02_Variables_and_Data_Types.py

--- 01_python_fundamentals/daily_practice/Day02_Variables_and_Data_Types.py
+++ 01_python_fundamentals/daily_practice/Day02_Variables_and_Data_Types.py
@@ -1,1 +1,9 @@
+name = "Roshan"
+age = 21
+print(name)
+print(age)
 
+name = "Roshan.R"
+age = 20
+print(f"{name}\n{age}") #prints the stored value in the variables in 2 different lines
+rint(f"name\nage") #prints the words "name","age" in 2 different lines 
