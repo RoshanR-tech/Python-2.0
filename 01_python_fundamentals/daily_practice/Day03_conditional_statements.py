472233ead94c3e9fde9589e348c2cdbb71c8543a
Roshan.R <roshanleo953@gmail.com>
2026-09-15 09:19:41 +0530

    Update Day03_conditional_statements.py

--- 01_python_fundamentals/daily_practice/Day03_conditional_statements.py
+++ 01_python_fundamentals/daily_practice/Day03_conditional_statements.py
@@ -4,3 +4,9 @@ if is_eligible :
     print("you can watch the movie.")
 else :
     print("you cannot watch this movie.")
+
+
+age = 16
+print(age >= 18)
+print(age < 18)
+print(age == 16)  #only prints true or false (boolean)
