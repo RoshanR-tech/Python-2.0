7b45284d8b8683312714e1613e31d5dc0a16261f
Roshan.R <roshanleo953@gmail.com>
2026-09-15 09:17:38 +0530

    Create Day03_conditional_statements.py

---
+++ 01_python_fundamentals/daily_practice/Day03_conditional_statements.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age:"))  #boolean data type example 
+is_eligible = age>=18
+if is_eligible :
+    print("you can watch the movie.")
+else :
+    print("you cannot watch this movie.")
