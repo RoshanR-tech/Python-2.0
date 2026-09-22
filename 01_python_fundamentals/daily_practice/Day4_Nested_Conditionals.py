8c198f829efb587befdb630ba36b1fabcfd41dec
Roshan.R <roshanleo953@gmail.com>
2026-09-22 23:29:24 +0530

    Update Day4_Nested_Conditionals.py

--- 01_python_fundamentals/daily_practice/Day4_Nested_Conditionals.py
+++ 01_python_fundamentals/daily_practice/Day4_Nested_Conditionals.py
@@ -8,3 +8,16 @@ if age >= 18 :
         print("Invalid ID")
 else :
     print("under age")
+
+
+age = int(input("Enter your age: "))
+if age >= 18 :
+    ID = 6769
+    valid_ID = input("enter your valid ID: ")
+    if valid_ID == ID :
+        print("Valid ID")
+    else :
+        print("Invalid ID")
+else :
+    print("under age")     #>enter 6769 , it says invalid . because ID is an integer data type and "input()" will take in the data as string 
+                           #>so we use "int(input())" to convert it into integer data type
