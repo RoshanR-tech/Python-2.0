d16ec8ccdb586d287f40158a46a05a4ec00cf5e4
Roshan.R <roshanleo953@gmail.com>
2026-09-22 23:25:54 +0530

    Update Day4_Nested_Conditionals.py

--- 01_python_fundamentals/daily_practice/Day4_Nested_Conditionals.py
+++ 01_python_fundamentals/daily_practice/Day4_Nested_Conditionals.py
@@ -1,1 +1,10 @@
-
+age = int(input("Enter your age: "))
+if age >= 18 :
+    ID = 6769
+    valid_ID = int(input("enter your valid ID: "))
+    if valid_ID == ID :
+        print("Valid ID")
+    else :
+        print("Invalid ID")
+else :
+    print("under age")
